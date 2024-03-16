1bea2037221fa14a39e2886ddab32bb3d59de492
yami1323 <163643109+yami1323@users.noreply.github.com>
2024-03-16 03:25:06 -0700

    Add files via upload

---
+++ patternprint.py
@@ -0,0 +1,39 @@
+
+# for i in range(1 , 6):
+#     for j in range(i):
+#         print("*" , end="")
+#     print()
+
+
+
+# rows = 4
+# n = 1
+# for i in range(1 , rows + 1):
+#     for j in range (1 , i + 1):
+#         print(n , end="")
+#         n += 1
+#     print()
+
+
+# r = 4
+# for i in range(r):
+#     for j in range(i+1):
+#         print(chr(j+65),end="")
+#     print()
+ 
+# char_value = 65
+# for i in range(1,5):
+#     for j in range(1 ,i+1):
+#         alphabet = chr(char_value)
+#         print(alphabet, end="")
+#         char_value = char_value+1
+#     print()
+
+# 
+
+
+rows = 4
+s = 2 * rows - 2
+for i in range(0,rows):
+    for j in range(0,s):
+        
