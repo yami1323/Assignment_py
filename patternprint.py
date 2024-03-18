2e5a7bd89fa1c334bddc9278601e3fe7ab29f64f
YAMI <163643109+yami1323@users.noreply.github.com>
2024-03-18 00:06:07 -0700

    Update patternprint.py

--- patternprint.py
+++ patternprint.py
@@ -29,7 +29,20 @@
 #         char_value = char_value+1
 #     print()
 
-# 
+# rows = 5
+
+# k = 0
+
+# for i in range(1, rows+1):
+#     for space in range(1, (rows-i)+1):
+#         print(end="  ")
+   
+#     while k!=(2*i-1):
+#         print("# ", end="")
+#         k += 1
+   
+#     k = 0
+#     print()
 
 
 
